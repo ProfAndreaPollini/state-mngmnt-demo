d24c8ac450988fee0ba6e957e1db625d2fa81e82
Andrea Pollini <prof.andrea.pollini@gmail.com>
2020-12-15 10:42:29 +0100

    codice prodotto a lezione

--- src/index.js
+++ src/index.js
@@ -1,12 +1,58 @@
-import React from "react"
+import React,{ useContext, useReducer} from "react"
 import ReactDOM from "react-dom"
 
 
 console.log("ok!")
 
+const DemoContext = React.createContext(null)
+
+function Demo() {
+	const dispatch = useContext(DemoContext)
+
+	function demoCmd() {
+		dispatch({
+			type:"demo",
+			payload:{
+	
+			}
+		})
+	}
+
+	
+	return(
+		<div>
+			<p>ciao </p>
+			<button onClick={()=>{
+			demoCmd()
+			
+			}}>TEST</button>
+		</div>
+	)
+}
+
+function demoReducer(state,action) {
+	switch (action.type) {
+		case "demo":
+			return [...state,"prova"]
+			break;
+	
+		default:
+			return state
+	}
+}
+
 function  App() {
+	const [state,dispatch] = useReducer(demoReducer,[])
+	
+	
 	return (
-		<h1>titolo</h1>
+		<DemoContext.Provider value={dispatch}>
+		<div className="app">
+			<h1>titolo</h1>
+			<Demo/>
+			{state}
+		</div>
+		</DemoContext.Provider>
 	)
 }
 
